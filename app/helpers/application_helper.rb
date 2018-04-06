345193f4e48009d0bdb6af0c1cdf2b2d50b24f5d
skmt1111 <sk_1494@icloud.com>
2018-04-06 04:37:14 +0000

    add_twittercard_func

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def get_twitter_card_info(user)
+    twitter_card = {}
+    if user
+      twitter_card[:url] = 'http://lifetree.work/#{user.nickname}'
+      twitter_card[:title] = 'LifeTree'
+      twitter_card[:description] = "@#{user.name}さんのTree"
+      twitter_card[:image] = 'https://bitbucket.org/skmt1111/ubuntu_0316_lifetree/src/master/app/assets/images/logo.png'
+    else
+      twitter_card[:url] = 'http://lifetree.work/'
+      twitter_card[:title] = 'あなたの人生や目標を一本の木で表現しよう「LifeTree」'
+      twitter_card[:description] = 'これまでの人生やこれからの目標などを管理できるサービスです。'
+      twitter_card[:image] = 'https://bitbucket.org/skmt1111/ubuntu_0316_lifetree/src/master/app/assets/images/logo.png'
+    end
+    twitter_card[:card] = 'summary'
+    twitter_card[:site] = '@LifeTree'
+    twitter_card
+  end
 end
