138c8510512418bf78059bf8d78374c13d1ebfe6
skmt1111 <sk_1494@icloud.com>
2018-04-06 04:53:39 +0000

    future_twittercard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,13 +5,12 @@ module ApplicationHelper
       twitter_card[:url] = 'http://lifetree.work/#{user.nickname}'
       twitter_card[:title] = 'LifeTree'
       twitter_card[:description] = "@#{user.name}さんのTree"
-      twitter_card[:image] = 'https://bitbucket.org/skmt1111/ubuntu_0316_lifetree/src/master/app/assets/images/logo.png'
     else
       twitter_card[:url] = 'http://lifetree.work/'
       twitter_card[:title] = 'あなたの人生や目標を一本の木で表現しよう「LifeTree」'
       twitter_card[:description] = 'これまでの人生やこれからの目標などを管理できるサービスです。'
-      twitter_card[:image] = 'https://bitbucket.org/skmt1111/ubuntu_0316_lifetree/src/master/app/assets/images/logo.png'
     end
+    twitter_card[:image] = 'assets/logo.png'
     twitter_card[:card] = 'summary'
     twitter_card[:site] = '@LifeTree'
     twitter_card
